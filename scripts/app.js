5496dc9a453cf4e544855c0f9cfaaefd6e0cc747
n.luparev <nikita.luparev@gmail.com>
2023-05-16 23:40:32 +0500

    saving data

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,20 @@ import Menu from './Menu.js';
 import Order from './Order.js';
 import Router from './Router.js';
 
-window.addEventListener("DOMContentLoaded", () => {
-    Router.init();
-    Menu.load();
-    Order.render();
- } );
+// Request Persistent Storage
+(async function () {
+  if (navigator.storage && navigator.storage.persist) {
+    if (!(await navigator.storage.persisted())) {
+      const result = await navigator.storage.persist();
+      console.log(`Was Persistent Storage Request granted? ${result}`);
+    } else {
+      console.log(`Persistent Storage already granted`);
+    }
+  }
+})();
+
+window.addEventListener('DOMContentLoaded', () => {
+  Router.init();
+  Menu.load();
+  Order.render();
+});
