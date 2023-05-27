5312907457ab9b9253251356b9b739037a6c870e
Nik Luparev <nikita.luparev@gmail.com>
2023-05-27 23:09:39 +0500

    change menu's loading to IDB

--- scripts/Menu.js
+++ scripts/Menu.js
@@ -1,57 +1,85 @@
 import API from './API.js';
 
+const IDB_DBNAME = 'cm-menu';
+const IDB_VERSION = 1;
+const IDB_STORE = 'categories';
+
 const Menu = {
-    data: null,
-    load: async () => {
-        Menu.data = await API.fetchMenu();
-        Menu.render();
-    },
-    getProductById: async id => {
-        if (Menu.data==null) {
-            await Menu.load();
+  data: null,
+
+  openDB: async () => {
+    return await idb.openDB(IDB_DBNAME, IDB_VERSION, {
+      async upgrade(db) {
+        await db.createObjectStore(IDB_STORE, { keyPath: 'name' });
+      },
+    });
+  },
+
+  load: async () => {
+    // Cache First
+    const db = await Menu.openDB();
+
+    if ((await db.count('categories')) == 0) {
+      const data = await API.fetchMenu();
+      data.forEach((category) => db.add('categories', category));
+    }
+
+    Menu.data = await db.getAll('categories');
+    Menu.render();
+  },
+
+  getProductById: async (id) => {
+    if (Menu.data == null) {
+      await Menu.load();
+    }
+    for (let c of Menu.data) {
+      for (let p of c.products) {
+        if (p.id == id) {
+          return p;
         }
-        for (let c of Menu.data) {
-            for (let p of c.products) {
-                if (p.id==id) {
-                    return p;
-                }
-            }
-        }
-        return null;
-    },
-    render: () => {
-        let html = "";
-        for (let category of Menu.data) {
-            html += `
+      }
+    }
+    return null;
+  },
+  render: () => {
+    let html = '';
+    for (let category of Menu.data) {
+      html += `
                 <li>
                     <h3>${category.name}</h3>
                     <ul class='category'>
-                        ${
-                            category.products.map(p => `
+                        ${category.products
+                          .map(
+                            (p) => `
                                 <li>
                                     <article>
                                         <a href="#" 
-                                            onclick="Router.go('/product-${p.id}');event.preventDefault()">
+                                            onclick="Router.go('/product-${
+                                              p.id
+                                            }');event.preventDefault()">
                                             <img src="/data/images/${p.image}">
                                             <h4>${p.name}</h4>
-                                            <p class="price">$${p.price.toFixed(2)}<p>
+                                            <p class="price">$${p.price.toFixed(
+                                              2,
+                                            )}<p>
                                         </a>
                                     </article>
                                 </li>
-                            `).join("")
-                        }
+                            `,
+                          )
+                          .join('')}
                     </ul>
                 </li>`;
-        }
-        document.querySelector("#menu").innerHTML = html;
-    },
-    renderDetails: async id => {
-        const product = await Menu.getProductById(id);
-        if (product==null) {
-            console.log(`Product ${id} not found`);
-            return;
-        }
-        document.querySelector("#details article").innerHTML = ` 
+    }
+    document.querySelector('#menu').innerHTML = html;
+  },
+  renderDetails: async (id) => {
+    const product = await Menu.getProductById(id);
+    if (product == null) {
+      console.log(`Product ${id} not found`);
+      return;
+    }
+    document.querySelector('#details article').innerHTML = ` 
             <header>   
                 <a href="#" onclick="Router.go('/'); event.preventDefault()">&lt; Back</a>
                 <h2>${product.name}</h2>
@@ -60,10 +88,11 @@ const Menu = {
             <img src="/data/images/${product.image}">
             <p class="description">${product.description}</p>
             <p class="price">$ ${product.price.toFixed(2)} ea</p>
-            <button onclick="Order.add(${product.id}); Router.go('/order')">Add to cart</button>
+            <button onclick="Order.add(${
+              product.id
+            }); Router.go('/order')">Add to cart</button>
         `;
-    }
-
-}
+  },
+};
 
 export default Menu;
